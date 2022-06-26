e42a32a2dfb6c21421b5fdaf7d4b2fdb8ab18367
jmulla <jibran.mulla@outlook.com>
2022-06-26 18:13:51 -0500

    Files

---
+++ main.js
@@ -0,0 +1,53 @@
+document.write('<h1>Grand Theft Auto: Californian Virtual Tour</h1>');
+document.write('<br></br>');
+document.write('<h2>Journey through a virtual tour of California using GTA 5!</h2>');
+document.write('<br></br>');
+document.write('<h2>The Bixby Canyon Bridge</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Griffith Observatory</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Hollywood Sign</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Hollywood Bowl</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Chinese Theater</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Hollywood Roosevelt Hotel</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Whiskey A Go Go</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Santa Monica Pier</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Venice Beach Boardwalk</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>L.A. Forum</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Los Angeles River</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Abandon Salt Riviera</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+document.write('<h2>Salvation Mountain</h2>');
+document.write('<br></br>');
+document.write('<br></br>');
+
+
+
+
+
+
+
+
+
+
