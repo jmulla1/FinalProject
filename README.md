d456ed301d05a75ef7f9d0d831d5128c33210cf8
jmulla1 <105895033+jmulla1@users.noreply.github.com>
2022-06-30 10:46:23 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # FinalProject
-
-Jibran Mulla, Final Project Assignment, jibranimulla@lewisu.edu
-
-
-
-Final Project Proposal
-https://mango-mushroom-0f0320210.1.azurestaticapps.net/
--- main.js
+++ main.js
@@ -1,47 +1,42 @@
+function initMap() {
+    gMap = new google.maps.Map(document.getElementById('myMapID'), {
+        center: {lat: 37.0902, lng: -95.7129}, zoom: 3});
+    }
+
+
+
+
+    
+
 document.write('<h1>Grand Theft Auto: Californian Virtual Tour</h1>');
 document.write('<br></br>');
 document.write('<h2>Journey through a virtual tour of California using GTA 5!</h2>');
 document.write('<br></br>');
-document.write('<h2>The Bixby Canyon Bridge</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Griffith Observatory</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Hollywood Sign</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Hollywood Bowl</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Chinese Theater</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Hollywood Roosevelt Hotel</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Whiskey A Go Go</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Santa Monica Pier</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Venice Beach Boardwalk</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>L.A. Forum</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Los Angeles River</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Abandon Salt Riviera</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
-document.write('<h2>Salvation Mountain</h2>');
-document.write('<br></br>');
-document.write('<br></br>');
 
+
+/*const image = new Image()
+image.src = 'https://i.ibb.co/0mS4SCT/Proj1.png'
+
+
+image.onload = function() {
+  drawCanvas(image)
+}
+
+function drawCanvas(image) {
+ 
+  const canvas = document.getElementById('floornet')
+  const context = canvas.getContext("2d")
+
+  canvas.width = image.height 
+  canvas.height = image.height 
+
+
+  context.drawImage(image, 0, 0) 
+}
+*/
+function initApplication() {
+    console.log('Final Project Starting!');
+}
 
 
 
